6a6424a76a6a94857f4a20458504a23762bfe3c8
Adler Medrado <adlermedrado@gmail.com>
2018-12-24 12:44:34 -0200

    Disable Flake8 errors

--- tests/test_abbr.py
+++ tests/test_abbr.py
@@ -19,7 +19,7 @@ from abbr.exceptions import LanguageNotFoundError, LengthNotFoundError, LengthSi
 
 
 def test_abbreviate_params():
-    phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"
+    phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"  # noqa
     abbr = Abbreviate(phrase, 40, 'pt_br')
     assert abbr.phrase == phrase
     assert abbr.language == 'pt_br'
@@ -34,24 +34,24 @@ def test_abbreviate_params_phrase_not_null_exception():
 
 def test_abbreviate_params_language_not_null_exception():
     with pytest.raises(LanguageNotFoundError):
-        phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"
+        phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"  # noqa
         Abbreviate(phrase, 40, '')
 
 
 def test_abbreviate_params_length_not_null_exception():
     with pytest.raises(LengthNotFoundError):
-        phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"
+        phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"  # noqa
         Abbreviate(phrase, '', 'pt_br')
 
 
 def test_abbreviate_params_length_must_be_greater_than_zero():
     with pytest.raises(LengthSizeError):
-        phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"
+        phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"  # noqa
         Abbreviate(phrase, 0, 'pt_br')
 
 
 def test_abbreviation_replace():
-    phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"
+    phrase = "Olá Senhor Professor Doutor Jair de Lima Pinto Sobrinho, como vai, tudo bem?"  # noqa
     abbr = Abbreviate(phrase, 40, 'pt_br')
     abbr.do_abbreviation()
     abbr.truncate_phrase()
